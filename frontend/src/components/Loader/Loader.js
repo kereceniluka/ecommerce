538137718d106b2a9268a57fed8a79a10be4d265
Luka Kerečeni <luka.kereceni@gmail.com>
2020-10-30 13:08:09 +0100

    Added Loader & Message components

---
+++ frontend/src/components/Loader/Loader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Loader from 'react-loader-spinner';
+
+const LoaderSpinner = () => {
+    return (
+        <Loader 
+            type="TailSpin"
+            color="#996faf"
+            width={100}
+            height={100}
+            style={{
+                margin: 'auto'
+            }}
+        />
+    );
+}
+
+export default LoaderSpinner;
